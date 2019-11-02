cb4dbbf2b44a373a67649cef81dd12bca3924bc5
theNerd247 <noah.harvey247 gm>
2019-11-02 01:32:22 -0400

    upates to 0.2.0.0

--- default.nix
+++ default.nix
@@ -3,12 +3,11 @@
 }:
 mkDerivation {
   pname = "xrandr";
-  version = "0.1.0.1";
+  version = "0.2.0.0";
   src = ./.;
   libraryHaskellDepends = [
     attoparsec base recursion-schemes shelly text
   ];
-  doHaddock = false;
   license = "unknown";
   hydraPlatforms = stdenv.lib.platforms.none;
 }
