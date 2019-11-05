4ee966a9939273a8474a3d240d8e7212797863a1
theNerd247 <noah.harvey247 gm>
2019-11-05 00:08:32 -0500

    finished more general runXrandr

--- src/Xrandr.hs
+++ src/Xrandr.hs
@@ -1,8 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ExplicitForAll#-}
+{-# LANGUAGE RankNTypes #-}
+
+
 
 module Xrandr 
-  ( runXrandr
-  , module Xrandr.Types
+  ( -- runXrandr
+    module Xrandr.Types
   )
 where
 
@@ -10,21 +15,23 @@ import Control.Monad ((>=>))
 import Shelly (shelly, run, run_)
 import qualified Data.Text as T
 import Control.Monad.IO.Class
+import Control.Monad.Fail
+import Control.Arrow
 
 import Xrandr.Cmd
 import Xrandr.Types
 import Xrandr.Parser
 
-runXrandr :: (MonadIO m) => ScreenCmd (OutputName, Cmd) -> m ()
-runXrandr = liftIO 
-  . (readCurrentScreens >>=) 
-  . either print 
-  . ((xrandr_ . snd).) 
-  . fromScreens 
-  . (makeCmd.)
+runXrandr :: (forall a. ScreenCmd a) -> Screens -> IO Screens
+runXrandr f = 
+    ((xrandr_ . snd . snd) *> (return . fst)) 
+  . (fromScreens $ bothAlg (toScreens . f) (makeCmd . f))
 
 readCurrentScreens :: IO (Either String Screens)
 readCurrentScreens = parseScreens <$> (xrandr [])
+
+bothAlg :: (Functor f) => (f a -> a) -> (f b -> b) -> f (a,b) -> (a,b)
+bothAlg f g = (f . fmap fst) &&& (g . fmap snd)
 
 xrandr_ :: Cmd -> IO ()
 xrandr_ c = xrandr c >> return ()
